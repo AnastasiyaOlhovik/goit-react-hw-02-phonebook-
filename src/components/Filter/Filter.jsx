664a82150240c9b9d3aadba0fc6e346b57210b10
Anastasiya Olhovik <93829935+AnastasiyaOlhovik@users.noreply.github.com>
2022-10-15 21:38:01 +0300

    Task2

---
+++ src/components/Filter/Filter.jsx
@@ -0,0 +1,23 @@
+import PropTypes from 'prop-types';
+import { Component } from 'react';
+
+class Filter extends Component {
+    setFilterValue = event => {
+        let value = event.currentTarget.value.toUpperCase();
+        this.props.setFilterToState(value);
+    };
+
+    render() {
+        return (
+            <div>
+                <h4>Find contacts by name</h4>
+                <input onChange={this.setFilterValue}></input>
+            </div>
+        );
+    }
+}
+
+Filter.propTypes = {
+    setFilterToState: PropTypes.func.isRequired,
+};
+export default Filter;
